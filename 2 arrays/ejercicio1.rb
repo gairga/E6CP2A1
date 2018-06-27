b3843e5528d9d2a299152f1d3eb11ccbf1c441ad
G@&R <gabrielailarreta@gmail.com>
2018-06-27 17:46:39 -0400

    correcciones 1 array

--- 2 arrays/ejercicio1.rb
+++ 2 arrays/ejercicio1.rb
@@ -6,3 +6,20 @@
 # 5. Mostrar en pantalla todos los elementos que se encuentren en una posición par.
 
 arreglo = [1,2,3,9,1,4,5,2,3,6,6]
+
+# 1. Mostrar en pantalla el primer elemento.
+puts arreglo.first  
+
+# 2. Mostrar en pantalla el último elemento.
+puts arreglo.last  
+# 3. Mostrar en pantalla todos los elementos.
+
+# 4. Mostrar en pantalla todos los elementos junto con un índice.
+
+
+# 5. Mostrar en pantalla todos los elementos que se encuentren en una posición par. 
+
+while x < arreglo.length
+  suma = suma + arreglo[x]
+  x = x + 1
+end
