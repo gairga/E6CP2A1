75f65b9bb928c3e03ceb17b6d57021eff87fd291
G@&R <gabrielailarreta@gmail.com>
2018-06-27 16:39:49 -0400

    Correccion ejercicio 4

--- 1 metodos/ejercicio4.rb
+++ 1 metodos/ejercicio4.rb
@@ -1,3 +1,14 @@
 # Crear un método que muestre en pantalla un saludo, el método
 # debe recibir un parámetro, si ese parámetro es el string "Hola" el
 # método debe mostrar en pantalla "Hola Mundo".
+def check5(var2)
+  var1="Hola"
+  result=var1.eql?(var2)  
+  if result == true
+      return "HOLA MUNDO"
+  else
+      return "Ingresa HOLAA"
+  end
+end
+
+puts check5("Hola")
