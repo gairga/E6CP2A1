f3b35d921dd47ddd07a8a0cee806a2e793b04f6c
G@&R <gabrielailarreta@gmail.com>
2018-06-27 17:10:09 -0400

    Método números pares

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,12 @@
 # Crear un método que reciba como parámetro dos números enteros positivos
 # e imprima los números pares que existen entre esos dos números.
+
+def numeros_pares(num1, num2)
+  for i in num1..num2
+    if i.even? 
+        puts "es un número par: #{i}"
+    end
+  end
+end
+
+numeros_pares(1,10)
